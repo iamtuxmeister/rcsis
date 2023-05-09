77242590f2cd8cf4764b485483f60e7b8823cb96
Kyle Stubblefield <iamtuxmeister@gmail.com>
2023-05-09 06:42:27 -0700

    added template and routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Heading, P } from 'flowbite-svelte';
+</script>
+
+<Heading class="p-8" tag="h1" customSize="text-3xl">Logged in index</Heading>
+<P class="px-8 py-4">
+	This is a dashboard landing page with information and quicklinks for the particular role
+</P>
---
+++ src/routes/gradebook/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Heading, P } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+	console.log(page);
+</script>
+
+<Heading class="p-8" tag="h1" customSize="text-3xl">Hello from the Gradebook</Heading>
+<P class="px-8 py-4">Gradebooky Stuff</P>
