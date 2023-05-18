7c846833e628945eb27e20b3bca301690f617dd6
Kyle Stubblefield <iamtuxmeister@gmail.com>
2023-05-18 06:16:27 -0700

    azure-ad auth working kindof

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,33 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
+import AzureAd from '@auth/core/providers/azure-ad';
 import GitHub from '@auth/core/providers/github';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { AUTH_SECRET, GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import {
+	AZURE_AD_CLIENT_ID,
+	AZURE_AD_TENANT_ID,
+	AZURE_AD_CLIENT_SECRET,
+	AUTH_SECRET,
+	GITHUB_ID,
+	GITHUB_SECRET
+} from '$env/static/private';
 import { prisma } from '$lib/server/prisma';
 
 export const handle = SvelteKitAuth({
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	adapter: PrismaAdapter(prisma),
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+	providers: [
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		AzureAd({
+			clientId: AZURE_AD_CLIENT_ID,
+			clientSecret: AZURE_AD_CLIENT_SECRET,
+			tenantId: AZURE_AD_TENANT_ID
+		})
+	],
 	secret: AUTH_SECRET,
 	trustHost: true
 });
